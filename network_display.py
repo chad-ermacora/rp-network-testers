916b8514ae38564b2df7397d3a234d8f23fab927
chad-ermacora <chad.e@linux.com>
2018-12-06 16:32:29 -0800

    Refactoring

--- network_display.py
+++ network_display.py
@@ -1,6 +1,6 @@
-'''
+"""
     KootNet Network Testers is a programs to test a Network Connection
-    Copyright (C) 2018  Chad Ermacora  chad.ermacora@gmail.com  
+    Copyright (C) 2018  Chad Ermacora  chad.ermacora@gmail.com
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,34 +14,53 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-'''
-import epsimplelib, os, socket
-import RPi.GPIO as GPIO
+"""
+import os
+import socket
 from time import sleep, strftime
 
-GPIO.setmode(GPIO.BCM)
+import RPi.GPIO as GPIO
+
+import epsimplelib
+
+mtr_cli_command = "mtr -c 10 -r -n 192.168.169.251 > /home/pi/MTR_Results.txt"
+iperf_cli_command = "iperf3 -c 192.168.169.251 -O 1 -p 9000 > /home/pi/iperf_Results.txt"
+start_message = "Device Ready\n\n" + \
+                "Be sure to\n" + \
+                "Give 15 Seconds\n" + \
+                "For Remote\n" + \
+                "Device to boot\n" + \
+                "Time: " + str(strftime("%H:%M"))
+
+
+def esp_message(display_message):
+    esp.add_text((1, 1), display_message)
+    esp.update_screen()
+    print(display_message)
+
+
+def run_cli_command(command):
+    os.system(command)
+
+
+def get_text_file_content(file_location):
+    local_file = open(file_location, "r")
+    return local_file.read()
+
+
 key1 = 5
 key2 = 6
 key3 = 13
 key4 = 19
 
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(key1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(key2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(key3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(key4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+esp = epsimplelib.EPScreen("portrait")  # eps = e-Ink Paper Screen
 
-esp = epsimplelib.EPScreen('portrait') # eps = e-Ink Paper Screen
-esp2 = epsimplelib.EPScreen('portrait') # eps = e-Ink Paper Screen
-
-def esp_message(message1):
-    esp.add_text((1,1), message1)
-    esp.update_screen()
-
-def esp_message2(message2):
-    esp2.add_text((1,1), message2)
-    esp2.update_screen()
-
-esp_message("Device Ready\n\nBe sure to\nGive 15 Seconds\nFor Remote\nDevice to boot\nTime: " + str(strftime("%H:%M")))
+esp_message(start_message)
 
 while True:
     key1state = GPIO.input(key1)
@@ -49,67 +68,70 @@ while True:
     key3state = GPIO.input(key3)
     key4state = GPIO.input(key4)
 
-    if key1state == False:
-        esp = epsimplelib.EPScreen('portrait') # eps = e-Ink Paper Screen
-        esp2 = epsimplelib.EPScreen('portrait') # eps = e-Ink Paper Screen
+    if not key1state:
         print("Key1 Pressed")
         esp_message("Starting MTR\nPlease Wait ...")
-
         try:
+            run_cli_command(mtr_cli_command)
+            cli_results = get_text_file_content("/home/pi/MTR_Results.txt")
+            message = "MTR Results\n" + \
+                      "Sent: " + str(cli_results)[-34:-30] + "\n" + \
+                      "Loss: " + str(cli_results)[-43:-35] + "\n" + \
+                      "Avg: " + str(cli_results)[-23:-18] + "ms" + "\n" + \
+                      "Worst: " + str(cli_results)[-10:-6] + "ms" + "\n" + \
+                      "Best: " + str(cli_results)[-17:-12] + "ms" + "\n" + \
+                      "Last: " + str(cli_results)[-29:-24] + "ms" + "\n" + \
+                      "StDev: " + str(cli_results)[-5:-1] + "ms" + "\n\n" + \
+                      " Day/Month/Year" + "\n\n" + \
+                      "Date: " + str(strftime("%d/%m/%y")) + "\n" + \
+                      "Time: " + str(strftime("%H:%M"))
+            esp_message(message)
+        except ConnectionError:
+            esp_message("MTR Failed\nUnit Offline?\nTime: " + str(strftime("%H:%M")))
+        else:
+            esp_message("Unknown Error")
 
-            os.system("mtr -c 10 -r -n 192.168.169.251 > /home/pi/MTR_Results.txt")
-            localfile = open("/home/pi/MTR_Results.txt", "r")
-            mess = localfile.read()
-            message = "MTR Results\nSent: " + str(mess)[-34:-30] + "\nLoss: " + str(mess)[-43:-35] + \
-                      "\nAvg: " + str(mess)[-23:-18] + "ms" + "\nWorst: " + str(mess)[-10:-6] + \
-                      "ms" + "\nBest: " + str(mess)[-17:-12] + "ms" + "\nLast: " + str(mess)[-29:-24] + \
-                      "ms" + "\nStDev: " + str(mess)[-5:-1] + "ms" + "\n\n Day/Month/Year" + "\n\nDate: " + \
-                      str(strftime("%d/%m/%y")) + "\nTime: " + str(strftime("%H:%M"))
-
-            print(message)
-            esp_message2(message)
-        except:
-            esp_message2("MTR Failed\nUnit Offline?\nTime: " + str(strftime("%H:%M")))
-
-    elif key2state == False:
-        esp = epsimplelib.EPScreen('portrait') # eps = e-Ink Paper Screen
-        esp2 = epsimplelib.EPScreen('portrait') # eps = e-Ink Paper Screen
-        print('Key2 Pressed')
+    elif not key2state:
+        print("Key2 Pressed")
         esp_message("Starting iPerf\nPlease Wait ...")
-
         try:
+            cli_results = get_text_file_content("/home/pi/iperf_Results.txt")
+            message = "iPerf3 Results\n" + \
+                      "Max device\n" + \
+                      "Bandwidth\n" + \
+                      " 220Mbps-230Mbps\n\n" + \
+                      "Transferred:\n" + \
+                      str(cli_results)[-68:-55] + "\n" + \
+                      "Bandwidth:\n" + \
+                      str(cli_results)[-55:-40] + "\n" + \
+                      "Over " + str(cli_results)[-79:-68] + "\n\n" + \
+                      " Day/Month/Year" + "\n" + \
+                      "Date: " + str(strftime("%d/%m/%y")) + "\n" + \
+                      "Time: " + str(strftime("%H:%M"))
+            esp_message(message)
+        except ConnectionError:
+            esp_message("iPerf3 Failed\nUnit Offline?\nTime: " + str(strftime("%H:%M")))
+        else:
+            esp_message("Unknown Error")
 
-            os.system("iperf3 -c 192.168.169.251 -O 1 -p 9000 > /home/pi/iperf_Results.txt")
-            localfile = open("/home/pi/iperf_Results.txt", "r")
-            mess = localfile.read()
-            message = "iPerf3 Results\nMax device\nBandwidth\n 220Mbps-230Mbps\n\nTransfered:\n" + \
-                      str(mess)[-68:-55] + "\nBandwidth:\n" + str(mess)[-55:-40] + "\nOver " + \
-                      str(mess)[-79:-68] + "\n\n Day/Month/Year" + "\nDate: " + str(strftime("%d/%m/%y")) + \
-                      "\nTime: " + str(strftime("%H:%M"))
-
-            print(message)
-            esp_message2(message)
-        except:
-            esp_message2("iPerf3 Failed\nUnit Offline?\nTime: " + str(strftime("%H:%M")))
-
-    elif key3state == False:
-        esp = epsimplelib.EPScreen('portrait') # eps = e-Ink Paper Screen
+    elif not key3state:
+        print("Key3 Pressed")
         server_address = ('192.168.169.251', 10062)
-        print('Key3 Pressed')
         try:
             sockG = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sockG.connect(server_address)
             esp_message("Shutting Down\nRemote Unit\nTime: " + str(strftime("%H:%M")))
-        except:
+        except Exception as error:
             esp_message("Shut Down Failed\nRemote N/A\nOffline : " + str(strftime("%H:%M")))
+            print(str(error))
 
-    elif key4state == False:
-        esp = epsimplelib.EPScreen('portrait') # eps = e-Ink Paper Screen
-        print('Key4 Pressed')
+    elif not key4state:
+        print("Key4 Pressed")
         try:
             esp_message("Shutting Down\nLocal Unit\nTime: " + str(strftime("%H:%M")))
             os.system("shutdown now -h")
-        except:
+        except Exception as error:
             esp_message("Shut Down Failed?\nTime: " + str(strftime("%H:%M")))
+            print(str(error))
 
     sleep(1)
--- network_server.py
+++ network_server.py
@@ -1,6 +1,6 @@
-'''
+"""
     KootNet Network Testers is a programs to test a Network Connection
-    Copyright (C) 2018  Chad Ermacora  chad.ermacora@gmail.com  
+    Copyright (C) 2018  Chad Ermacora  chad.ermacora@gmail.com
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,38 +14,32 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-'''
+"""
 
-import socket, os
+import socket
+import os
 from time import sleep
 
-network_wait = 0
+network_wait = True
 
-while network_wait == 0:
+while network_wait:
     try:
-        # Create a TCP/IP socket and Bind the socket to the port
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_address = ('192.168.169.251', 10062)
         print('starting up on {} port {}'.format(*server_address))
         sock.bind(server_address)
-        network_wait = 1
-    except:
+        sock.listen(1)
+        while True:
+            print('\nwaiting for a connection ... \n')
+            connection, client_address = sock.accept()
+            try:
+                print('connection from', client_address)
+                os.system("sudo shutdown -h now")
+            except Exception as error:
+                print("Connection Failed: " + str(error))
+            finally:
+                # Clean up the connection
+                connection.close()
+    except Exception as error:
+        print(str(error))
         sleep(5)
-
-# Listen for incoming connections
-sock.listen(1)
-
-while True:
-    # Wait for a connection
-    print('\nwaiting for a connection ... \n')
-    connection, client_address = sock.accept()
-    try:
-        print('connection from', client_address)
-        os.system("sudo shutdown -h now")
-    except:
-        print("\nConnection Failed?\n")
-    finally:
-        # Clean up the connection
-        connection.close()
-# Close again, in case it didn't before... Should never reach this. 
-connection.close()
