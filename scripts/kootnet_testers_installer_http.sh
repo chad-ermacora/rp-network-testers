574ce7678f3f8bc50a024c6a73d06bd99b31b1e4
chad-ermacora <chad.ermacora@gmail.com>
2020-03-30 10:59:18 -0700

    Added apt-get update before installing deb update  In case there are new apt-get installs required, it needs an up to date list Added --reinstall for deb so it re-runs if the same version (re-install requirements)

--- scripts/kootnet_testers_installer_http.sh
+++ scripts/kootnet_testers_installer_http.sh
@@ -18,5 +18,7 @@ fi
 printf '\n-- %s HTTP UPGRADE OR INSTALL --\n' "${INSTALL_TYPE}"
 # Clean up previous downloads if any
 rm -f /tmp${DEB_INSTALLER} 2>/dev/null
+# Make sure apt-get is up-to-date (stale package lists can make upgrades and installs fail)
+apt-get update
 wget -O /tmp${DEB_INSTALLER} ${HTTP_SERVER}${HTTP_FOLDER}${DEB_INSTALLER}
-apt-get -y install /tmp${DEB_INSTALLER}
+apt-get -y install --reinstall /tmp${DEB_INSTALLER}
