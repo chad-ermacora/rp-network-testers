8f69402cf8531dfbbd63dcf4b87b30434a409d56
chad-ermacora <chad.ermacora@gmail.com>
2020-11-30 07:40:11 -0800

    Added run once at date/time code

--- operations_modules/schedule_server.py
+++ operations_modules/schedule_server.py
@@ -17,14 +17,44 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 from time import sleep
+from datetime import datetime
 from operations_modules.logger import primary_logger
+from operations_modules import app_variables
 from operations_modules.config_primary import current_config
 from operations_modules import run_commands
 
 
-def _start_run_at_date(run_on_date_time):
-    if current_config.schedule_run_on_boot:
-        pass
+def start_run_at_date():
+    app_variables.restart_run_once_test_server = 0
+    while not app_variables.restart_run_once_test_server:
+        while not current_config.schedule_run_once_enabled:
+            sleep(30)
+
+        try:
+            utc_now = datetime.utcnow()
+            scheduled_runtime = datetime.strptime(current_config.schedule_run_once_at_time, "%Y-%m-%dT%H:%M")
+            sleep_time = (scheduled_runtime - utc_now).total_seconds()
+
+            if sleep_time > 0:
+                total_sleep_time = 0
+                skip_test_runs = 0
+                primary_logger.info("Single Run Tests set to run in: " + str(sleep_time) + " Seconds")
+                while total_sleep_time < sleep_time:
+                    sleep(5)
+                    total_sleep_time += 5
+                    if app_variables.restart_run_once_test_server:
+                        skip_test_runs = 1
+                        sleep_time = total_sleep_time
+                if not skip_test_runs:
+                    run_commands.start_mtr()
+                    run_commands.start_iperf()
+            else:
+                primary_logger.warning("Single Run Tests Date & Time has already passed, disabling Run Once Schedule")
+                current_config.schedule_run_once_enabled = 0
+                current_config.write_config_to_file()
+        except Exception as error:
+            primary_logger.error("Single scheduled test run: " + str(error))
+            current_config.schedule_run_once_enabled = 0
 
 
 def start_run_every_minutes():
@@ -35,6 +65,10 @@ def start_run_every_minutes():
     if current_config.schedule_run_every_minutes < 5:
         primary_logger.warning("Scheduled times cannot be less then 5 minutes")
         current_config.schedule_run_every_minutes = 5
+
+    if current_config.schedule_run_every_minutes_enabled:
+        log_msg = "Tests scheduled to run every " + str(current_config.schedule_run_every_minutes) + " minutes"
+        primary_logger.info(log_msg)
 
     while True:
         try:
